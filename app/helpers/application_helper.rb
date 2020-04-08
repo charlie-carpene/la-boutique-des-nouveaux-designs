fa08c8e6289dd81a3107ee2e0438aecbc2016b97
Charlie Carpene <solunacisv@gmail.com>
2020-04-08 19:14:14 +0200

    changing Devise flash notice style and to french

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when 'notice'
+        "alert alert-primary"
+      when 'success'
+        "alert alert-success"
+      when 'error'
+        "alert alert-danger"
+      when 'alert'
+        "alert alert-danger"
+      else
+        "alert alert-info"
+    end
+  end
+
 end
