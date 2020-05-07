519703356a9d27cd3ef7df70a59cdce485828170
Charlie Carpene <solunacisv@gmail.com>
2020-05-07 17:20:30 +0200

    add translation to new_shop_form attribute

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,7 +2,7 @@ class Shop < ApplicationRecord
   validates :brand, presence: true
   validates :description, presence: true
   validates :email_pro, presence: true, format: { with: /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "doit être un site web valide" }
-  validates :website, presence: true, format: { with: /\A^(https?:\/\/)?(www\.)?([a-zA-Z0-9]+(-?[a-zA-Z0-9])*\.)+[\w]{2,}(\/\S*)?$\z/, message: "doit être un site web valide" }
+  validates :website, allow_blank: true, format: { with: /\A^(https?:\/\/)?(www\.)?([a-zA-Z0-9]+(-?[a-zA-Z0-9])*\.)+[\w]{2,}(\/\S*)?$\z/, message: "doit être un site web valide" }
 
   belongs_to :user
   has_one :address
