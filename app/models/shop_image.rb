b501c0a8aeeed63c57cf72e2ad107146bfeedb3b
Charlie Carpene <solunacisv@gmail.com>
2020-05-07 16:09:19 +0200

    allow uploads up to 7 files only

--- app/models/shop_image.rb
+++ app/models/shop_image.rb
@@ -1,4 +1,16 @@
 class ShopImage < ApplicationRecord
+  validate :images_limite_nbr
+
   include ImageUploader::Attachment(:image)
   belongs_to :shop
+
+  private
+
+  #method to limit the number of uploaded file to 7 (kbis, rib & up to 5 pictures) when a user fills the form to become a maker.
+  def images_limite_nbr
+    limit = 7
+    if self.shop.shop_images.size > limit
+      errors.add(:image, "must be lower than #{limit}")
+    end
+  end
 end
