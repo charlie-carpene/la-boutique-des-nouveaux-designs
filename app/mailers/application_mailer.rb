44a499ea5f3b9b6656dcfc938aa9b03f9d884c9e
Charlie Carpene <solunacisv@gmail.com>
2020-05-08 12:48:46 +0200

    attached files to  email when maker registration // WIP

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,16 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def add_attached_file(user)
+    "Attachments": [
+      user.shop.shop_images.each do |i|
+        {
+          "ContentType": "#{user.shop.shop_images[i].image.metadata['mime_type']}",
+          "Filename": "#{user.shop.shop_images[i].image.metadata['filename']}",
+          "Base64Content": "VGhpcyBpcyB5b3VyIGF0dGFjaGVkIGZpbGUhISEK"
+        }
+      end
+    ]
+  end
 end
