4fc6029b5e2ade87bb2cc2a10306e73d4b76ae6a
Charlie Carpene <solunacisv@gmail.com>
2020-05-09 14:50:32 +0200

    change region path in shrine.rb

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -11,7 +11,7 @@ else
   s3_options = {
     access_key_id:     Rails.application.secrets.s3_access_key_id,
     secret_access_key: Rails.application.secrets.s3_secret_access_key,
-    region:            Rails.application.secrets.s3_region,
+    region:            "eu-west-3",
     bucket:            "la-boutique-des-nouveaux-designs"
   }
 
