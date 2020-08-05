240df037b5a3b92b2449f566bd61d7695ed4c084
Charlie Carpene <solunacisv@gmail.com>
2020-08-06 00:46:02 +0200

    change seed, carrousel and item image to show image for fake items

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,18 +32,24 @@ User.create(email: "atelier@nouveauxdesigns.fr", password: "password", password_
 Admin.create(user: User.find_by(email: "atelier@nouveauxdesigns.fr"))
 Address.create(first_name: "Charlie", last_name: "Carpene", address_line_1: "31 rue du 4 aout 1789", address_line_2: "", zip_code: "69100", city: "Villeurbanne", user: User.find_by(email: "atelier@nouveauxdesigns.fr"))
 Address.create(first_name: "Jonas", last_name: "Thevenon", address_line_1: "Rue de panam", address_line_2: "", zip_code: "75020", city: "Paris", user: User.find_by(email: "atelier@nouveauxdesigns.fr"))
-Shop.create(brand: "ShowerChiottes", website: "www.showerchiott.es", email_pro: "atelier@nouveauxdesigns.fr", description: Faker::Quote.famous_last_words, user: User.find_by(email: "atelier@nouveauxdesigns.fr"))
+Shop.create(brand: "ShowerChiottes", website: "www.showerchiott.es", email_pro: "atelier@nouveauxdesigns.fr", description: Faker::Quote.famous_last_words, user: User.find_by(email: "atelier@nouveauxdesigns.fr"), compagny_id: Faker::Number.number(digits: 14))
 puts "... done"
 
 puts "***** Maker with address & shop ... *****"
 User.create(email: "gradya@yopmail.com", password: "password", password_confirmation: "password", is_maker: true)
-Shop.create(brand: "Gradya bg compagny", website: "www.showerchiott.es", email_pro: "gradya@yopmail.com", description: Faker::Quote.famous_last_words, user: User.find_by(email: "gradya@yopmail.com"))
+Shop.create(brand: "Gradya bg compagny", website: "www.showerchiott.es", email_pro: "gradya@yopmail.com", description: Faker::Quote.famous_last_words, user: User.find_by(email: "gradya@yopmail.com"), compagny_id: Faker::Number.number(digits: 14))
 Address.create(first_name: "Gradya", last_name: "Gradya", address_line_1: "13 rue des Gobelin", address_line_2: "", zip_code: "75005", city: "Paris", user: User.find_by(email: "gradya@yopmail.com"))
 puts "... done"
 
 puts "***** Common user ... *****"
 User.create(email: "gawain@yopmail.com", password: "password", password_confirmation: "password")
 Address.create(first_name: "Gawain", last_name: "Vincent", address_line_1: "20 rue des Gobelins", address_line_2: "", zip_code: "75005", city: "Paris", user: User.find_by(email: "gawain@yopmail.com"))
+puts "... done"
+
+puts "***** Items ... *****"
+10.times do
+  Item.create(name: Faker::Creature::Cat.name, category: Category.all.sample, description: Faker::TvShows::HowIMetYourMother.quote, price: Faker::Number.number(digits:2), available_qty: Faker::Number.number(digits:1), shop: Shop.all.sample)
+end
 puts "... done"
 
 puts "-" * 30
