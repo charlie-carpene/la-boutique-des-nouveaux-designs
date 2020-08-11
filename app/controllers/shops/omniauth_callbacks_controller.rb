8de493d943b2d7ea6f5084bb7c03d7f0fa49b01e
Charlie Carpene <solunacisv@gmail.com>
2020-08-11 21:34:16 +0200

    add error message to heroku logs for Stripe

--- app/controllers/shops/omniauth_callbacks_controller.rb
+++ app/controllers/shops/omniauth_callbacks_controller.rb
@@ -3,19 +3,17 @@ class Shops::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def stripe_connect
     auth_data = request.env["omniauth.auth"]
     @shop = current_user.shop
-    puts "-" * 30
-    puts "juste avant"
-    puts "-" * 30
+
     if @shop.persisted?
-      puts "*" * 30
-      puts "juste après"
-      puts "*" * 30
       @shop.provider = auth_data.provider
       @shop.uid = auth_data.uid
       @shop.access_code = auth_data.credentials.token
       @shop.publishable_key = auth_data.info.stripe_publishable_key
       @shop.save
-
+      puts "*" * 30
+      puts @shop.errors.messages
+      puts "*" * 30
+      
       sign_in_and_redirect @shop
       flash[:notice] = 'Votre compte Stripe a bien été créé et est maintenant connecté à votre boutique' if is_navigational_format?
     else
