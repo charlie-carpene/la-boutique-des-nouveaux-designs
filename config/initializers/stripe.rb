11af8d4289b3cf584cac254eac21c803e5cd1a17
Charlie Carpene <solunacisv@gmail.com>
2020-11-09 19:40:42 +0100

    delete services file with stripe event_handler

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,8 +4,3 @@ Rails.configuration.stripe = {
   :signing_secret  => ENV['STRIPE_SIGNING_SECRET']
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
-StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
-
-StripeEvent.configure do |events|
- events.subscribe 'charge.dispute.created', Stripe::EventHandler.new
-end
