04be825c43f2194792fd8191df9434894985cccb
Charlie Carpene <solunacisv@gmail.com>
2020-11-16 20:23:35 +0100

    add heroku.com in config/environnements/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,5 +63,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.hosts << "0dfc1c576433.ngrok.io"
+  config.hosts << "boutique-des-nouveaux-designs.herokuapp.com"
 end
