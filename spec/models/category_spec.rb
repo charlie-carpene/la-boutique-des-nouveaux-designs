002c593073e9a946838c46635f75ef2c4ae4e302
Charlie Carpene <solunacisv@gmail.com>
2021-05-19 17:16:52 +0200

    clean puts

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,20 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  before do
-    @shop = create(:shop)
-    @category_with_items = create(:category_with_items, items_count: 2, shop: @shop)
-    @categories = FactoryBot.create_list(:category, 3)
-    @item = create(:item, categories: [@categories[1]], shop: @shop)
-    puts "*"  *  30
-    puts Category.all.inspect
-    puts "*"  *  30
-    puts Item.all.inspect
-    puts "*"  *  30
-    puts Shop.all.inspect
-    puts "*"  *  30
-    puts User.all.inspect
-    puts "*"  *  30
+  let!(:shop) { create(:shop) }
+  let!(:category_with_items) { create(:category_with_items, items_count: 2, shop: shop) }
+  let!(:categories) { create_list(:category, 3) }
+  let!(:item) { create(:item, categories: [categories[1]], shop: shop) }
+
+  it 'should create a valid instance of Category' do
+    expect(category_with_items).to be_valid
   end
 
   context 'creation' do
@@ -22,14 +15,11 @@ RSpec.describe Category, type: :model do
       expect(Category.count).to eq(4)
     end
 
-    it 'needs to have a name' do
-      expect(@categories.sample.name).to be_kind_of(String)
-    end
-
-    it 'can have one or more item(s)' do      
-      expect(@categories[0].items.count).to eq(0)
-      expect(@categories[1].items.count).to eq(1)
-      expect(@category_with_items.items.count).to be > 1
+    it 'can have one or more item(s)' do
+      expect(category_with_items).to have_many(:items).through(:item_category)
+      expect(categories.first.items.count).to eq(0)
+      expect(categories.second.items.count).to eq(1)
+      expect(category_with_items.items.count).to be > 1
     end
   end
 end
