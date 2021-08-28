95cd9cd04ae76fd9e1fb5cf99c035ada4fddcdf5
Charlie Carpene <solunacisv@gmail.com>
2021-08-28 15:34:20 +0200

    add i18n for user and stripe controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @legal_compagny_name = @user_to_validate.shop.verify_company_id
 
     unless load_user.admin.present? #can't use load_resource because it takes the params from the URL and here we want the current_user id
-      flash[:error] = "Veuillez vous connecter en tant qu'administrateur pour accéder à cette page."
+      flash[:error] = t("errors.admin_required")
       redirect_to root_path
     end
   end
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user_to_validate.is_maker = true
     if load_user.admin.present? && @user_to_validate.save
       UserMailer.new_shop_request_accepted(@user_to_validate).deliver_now
-      flash[:success] = "La validation a bien été effectuée et un mail a été envoyé à #{@user_to_validate.shop.email_pro}."
+      flash[:success] = t("success.validate_user_as_maker", user_email: @user_to_validate.shop.email_pro)
       redirect_to root_path
     else
       flash[:error] = translate_error_messages(@user_to_validate.shop.errors)
