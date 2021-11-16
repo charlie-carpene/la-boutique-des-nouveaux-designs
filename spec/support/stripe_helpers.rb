c508f641247f476d1a0fdd51b3e969c827ddf2a3
Charlie Carpene <solunacisv@gmail.com>
2021-11-16 18:41:24 +0100

    create order request specs

--- spec/support/stripe_helpers.rb
+++ spec/support/stripe_helpers.rb
@@ -10,4 +10,16 @@ module StripeData
 			source: stripe_helper.generate_card_token
 		})
 	end
+	
+	def generate_stripe_event_signature(payload)
+    time = Time.now
+    secret = ENV['SECRET_KEY']
+    signature = Stripe::Webhook::Signature.compute_signature(time, payload, secret)
+    Stripe::Webhook::Signature.generate_header(
+      time,
+      signature,
+      scheme: Stripe::Webhook::Signature::EXPECTED_SCHEME
+    )
+	end
+
 end
