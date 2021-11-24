dbf231960b75536bc315fb182a6baa22cec0b84d
Charlie Carpene <solunacisv@gmail.com>
2021-11-24 16:02:25 +0100

    fix image uploading bug for item pictures

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,6 +7,7 @@ class PictureUploader < Shrine
   plugin :processing
   plugin :delete_raw
   plugin :derivatives, versions_compatibility: true
+  plugin :default_url
 
   Attacher.validate do
     validate_mime_type %w[image/jpeg image/png], message: "doit être un jpeg ou un png"
@@ -24,13 +25,17 @@ class PictureUploader < Shrine
     }
   end
 
+  Attacher.default_url do |derivative: nil, **|
+    file&.url if derivative
+  end
+
   def generate_location(io, context = {})
     extension = ".#{io.extension}" if io.is_a?(UploadedFile) && io.extension
     extension ||= File.extname(extract_filename(io).to_s).downcase
     @filename = File.basename(extract_filename(io).to_s, '.*').downcase.split(/[^a-zA-Z\d:]/).join
     version = context[:derivative].blank? ? 'original' : context[:derivative]
     itemname = context[:record].item.present? === true ? "#{context[:record].item.name.titleize.split(/[\s$&+,:;=?@#|'<>.^*()%!-]/).join}" : ''
-    item_id = context[:record].item.id
+    item_id = context[:record].item.present? === true ? context[:record].item.id : ''
     shopname = context[:record].item.present? === true ? "#{context[:record].item.shop.brand}/" : ''
     directory = context[:record].class.name.downcase.pluralize
     "#{directory}/#{shopname}#{itemname}_item-#{item_id}_#{@filename}_#{version}#{extension}"
