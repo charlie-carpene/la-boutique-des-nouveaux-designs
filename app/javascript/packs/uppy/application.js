2d443e459583114b46cf608d1f644fdfe50ad7b9
Charlie Carpene <solunacisv@gmail.com>
2021-12-27 16:36:27 +0100

    end of line

--- app/javascript/packs/uppy/application.js
+++ app/javascript/packs/uppy/application.js
@@ -1,6 +1,6 @@
-import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 
-window.Stimulus = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-Stimulus.load(definitionsFromContext(context))
+window.Stimulus = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+Stimulus.load(definitionsFromContext(context));
