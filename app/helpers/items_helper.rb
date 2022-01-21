c2bede2088fa45167f31e3ef6b716a702833e445
Charlie Carpene <solunacisv@gmail.com>
2022-01-21 15:22:39 +0100

    change Item img fallback method from Model to Helper

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,13 @@
 module ItemsHelper
+  def cover_picture_with_fallback(id, img_type)
+    @item = Item.find(id)
+
+    if @item.item_pictures.where(id: @item.cover_picture).exists?
+      return @item.item_pictures.find(@item.cover_picture).picture_url(img_type)
+    elsif @item.item_pictures.exists?
+      return @item.item_pictures.last.picture_url(img_type)
+    else
+      return ActionController::Base.helpers.asset_path("img-items/adnd-squarre-0.jpeg", :digest => false)
+    end
+  end
 end
