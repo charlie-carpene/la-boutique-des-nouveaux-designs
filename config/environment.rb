7523bc23f22e247aaa2680595cca0da6e4dd2839
Charlie Carpene <solunacisv@gmail.com>
2022-02-22 15:09:02 +0100

    delete heroku_env.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-# Load heroku vars from local file
-heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
-load(heroku_env) if File.exists?(heroku_env)
-
 # Initialize the Rails application.
 Rails.application.initialize!
