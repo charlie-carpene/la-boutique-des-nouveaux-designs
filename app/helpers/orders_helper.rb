04e88434a4e14bef3ef575f2475be552ace98ffa
Charlie Carpene <solunacisv@gmail.com>
2022-02-26 23:26:33 +0100

    correct error translations

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -17,7 +17,7 @@ module OrdersHelper
 
   def disabling_checkout_button
     if !@order.user.addresses.exists? || !@order.user.addresses.where(id: @order.user.delivery_address).exists?
-      @order.errors.add(:add_address, t("button.disabled.add_user_address"))
+      @order.errors.add("address.shipping_address", t("button.disabled.data_not_filled_in_f"))
       return true
     elsif !@ordered_cart_items.first.item.shop.address.present?
       @order.errors.add(:maker_shop, t("button.disabled.add_maker_address", shop: @ordered_cart_items.first.item.shop.brand))
